bf6aeae47084db97eb2ff4920ed4b880814c76eb
DiegoPillajo1989 <dps700@hotmail.com>
2020-08-06 17:02:50 -0500

    agregar camara

--- app/src/main/java/com/example/diegopillajo_examen/Registro.java
+++ app/src/main/java/com/example/diegopillajo_examen/Registro.java
@@ -12,65 +12,43 @@ import android.widget.Toast;
 public class Registro extends AppCompatActivity {
     TextView cajaRecibir;
     Bundle loginusuario;
-    TextView pago;
-    TextView cuota;
-    EditText monto;
     TextView nombre;
     TextView apellido;
     TextView direccion;
+    TextView telefono;
+    TextView pago;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         cajaRecibir=findViewById(R.id.tvBienvenido);
-        pago=findViewById(R.id.tVPago);
-        cuota=findViewById(R.id.tVCouta);
-        monto = findViewById(R.id.eTMonto);
         nombre = findViewById(R.id.eTNombre);
+        apellido = findViewById(R.id.eTApellido);
+        direccion = findViewById(R.id.eTDireccion);
+        telefono = findViewById(R.id.eTTelefono);
+        pago = findViewById(R.id.eTPago);
         //Recibo el usuario
         loginusuario = getIntent().getExtras();
         String datoRecibido = loginusuario.getString("datoEnviado");
         cajaRecibir.setText(datoRecibido);
-        //
-
     }
-  /*  public void Calcular(View view) {
-        if (monto.getText().toString().equals(""))
-        {
-            Toast.makeText(this,"Monto Incorrecto", Toast.LENGTH_LONG).show();
-        }
-        else {
-            double montoI, resultado, impuesto, pagos, coutas;
-            montoI = Double.parseDouble(monto.getText().toString());
-            resultado = 1800 - montoI;
-            impuesto = resultado * 0.05;
-            pagos = resultado + impuesto;
-            pago.setText(Double.toString(pagos));
-            coutas = pagos / 3;
-            cuota.setText(Double.toString(coutas));
-        }
-    }*/
     public void Guardar(View view) {
-       /* if (monto.getText().toString().equals("") || pago.getText().toString().equals("") || cuota.getText().toString().equals(""))
-        {
-            Toast.makeText(this,"Monto Incorrecto", Toast.LENGTH_LONG).show();
-        }
-        else
-        {*/
             Intent intentDatos = new Intent (Registro.this,Encuesta.class);
             intentDatos.putExtra("usuario",cajaRecibir.getText().toString());
             intentDatos.putExtra("nombre",nombre.getText().toString());
+            intentDatos.putExtra("apellido",apellido.getText().toString());
+            intentDatos.putExtra("direccion",direccion.getText().toString());
+            intentDatos.putExtra("telefono",telefono.getText().toString());
             intentDatos.putExtra("total",pago.getText().toString());
             startActivity(intentDatos);
             Toast.makeText(this,"Elemento Guardado con Éxito", Toast.LENGTH_LONG).show();
-        /*}*/
     }
     public void Limpiar(View view) {
         nombre.setText("");
         apellido.setText("");
-
+        direccion.setText("");
+        telefono.setText("");
         pago.setText("");
-        monto.setText("");
-        cuota.setText("");
+
     }
 }
